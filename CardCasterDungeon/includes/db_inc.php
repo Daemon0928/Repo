5ac5135ae203bc15c7ff263b01a2cd34e909de82
Mark Molnar <51020205+Daemon0928@users.noreply.github.com>
2022-10-29 15:52:33 +0200

    Delete CardCasterDungeon directory

--- CardCasterDungeon/includes/db_inc.php
+++
@@ -1,8 +0,0 @@
-<?php  
-$serverName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "cardcasterdungeon";
-
-$connection = mysqli_connect($serverName,$dbUsername,$dbPassword,$dbName);
-?>
